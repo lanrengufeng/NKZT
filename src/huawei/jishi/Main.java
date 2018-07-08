3f8c22038dd1730542436569521c27a30c2d51e1
Watcher <lanrengufeng@163.com>
2018-07-08 17:35:40 +0800

    新增华为机试第6题

--- src/huawei/jishi/Main.java
+++ src/huawei/jishi/Main.java
@@ -4,18 +4,78 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-
-		hexToDec();
+		Main mm = new Main();
+		Scanner in = new Scanner(System.in);
+		long num = in.nextLong();
+		
+		System.out.println(mm.getResult(num));
+		// hexToDec();
 
 		// splitStrings2();
 		// splitStrings();
 		// lastStringLength();
 		// charNum();
+		in.close();
 	}
 
 	/**
-	 * 5
-	 * 题目描述 写出一个程序，接受一个十六进制的数值字符串，输出该数值的十进制字符串。（多组同时输入 ）
+	 * 题目描述 功能:输入一个正整数，按照从小到大的顺序输出它的所有质数的因子（如180的质数因子为2 2 3 3 5 ）
+	 * 
+	 * 最后一个数后面也要有空格
+	 * 
+	 * 详细描述：
+	 * 
+	 * 
+	 * 函数接口说明：
+	 * 
+	 * public String getResult(long ulDataInput)
+	 * 
+	 * 输入参数：
+	 * 
+	 * long ulDataInput：输入的正整数
+	 * 
+	 * 返回值：
+	 * 
+	 * String
+	 * 
+	 * 
+	 * 
+	 * 输入描述: 输入一个long型整数
+	 * 
+	 * 输出描述: 按照从小到大的顺序输出它的所有质数的因子，以空格隔开。最后一个数后面也要有空格。
+	 * 
+	 * @param ulDataInput
+	 * @return
+	 */
+	public String getResult(long ulDataInput) {
+		if(ulDataInput<=1)
+			return "";
+		String res = "";
+		long num = ulDataInput;
+		while(num!=1){
+			for(int i=2;i<=num;i++){
+				if(num%i==0){
+					num/=i;
+					res = res+i+" ";
+					break;
+				}
+			}
+		}
+		return res;
+	}
+	
+	public boolean isPrime(long num){
+		if(num<2)
+			return false;
+		for(int i=2;i<num;i++){
+			if(num%i==0)
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 5 题目描述 写出一个程序，接受一个十六进制的数值字符串，输出该数值的十进制字符串。（多组同时输入 ）
 	 * 
 	 * 输入描述: 输入一个十六进制的数值字符串。
 	 * 
