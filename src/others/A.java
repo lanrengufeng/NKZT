b8d5ddf97afe5961fcfaef8d1f3e5e832084def2
watcher <lanrengufeng@qq.com>
2018-07-08 21:28:20 +0800

    测试一下构造函数

---
+++ src/others/A.java
@@ -0,0 +1,21 @@
+package others;
+
+/**
+ * 验证初始化顺序
+ * @author Watcher
+ *
+ */
+public class A {
+	A a = new A(1);
+	public A(int a){
+		System.out.println(a);
+	}
+	
+	//A a = new A(1);
+	static A aa =  new A(2);
+	public static void main(String[] args) {
+		System.out.println("main");
+		new A(3);
+		
+	}
+}
