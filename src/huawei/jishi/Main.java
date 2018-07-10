05c06b5873a9ac76e02ead6971a153aeeef5e9af
Watcher <lanrengufeng@163.com>
2018-07-10 13:41:12 +0800

    华为机试题

--- src/huawei/jishi/Main.java
+++ src/huawei/jishi/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Map.Entry;
-
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
@@ -14,7 +13,9 @@ public class Main {
 	public static void main(String[] args) {
 		Main mm = new Main();
 
-		mm.maxValue();
+		mm.moveCoordinate();
+
+		// mm.maxValue();
 
 		// mm.numOf1();
 
@@ -47,6 +48,118 @@ public class Main {
 		// lastStringLength();
 		// charNum();
 		// in.close();
+	}
+
+	/**
+	 * 开发一个坐标计算工具， A表示向左移动，D表示向右移动，W表示向上移动，S表示向下移动。从（0,0）点开始移动，从输入字符串里面读取一些坐标，
+	 * 并将最终输入结果输出到输出文件里面。
+	 * 
+	 * 
+	 * 
+	 * 输入：
+	 * 
+	 * 
+	 * 
+	 * 合法坐标为A(或者D或者W或者S) + 数字（两位以内）
+	 * 
+	 * 
+	 * 
+	 * 坐标之间以;分隔。
+	 * 
+	 * 
+	 * 
+	 * 非法坐标点需要进行丢弃。如AA10; A1A; $%$; YAD; 等。
+	 * 
+	 * 
+	 * 
+	 * 下面是一个简单的例子 如：
+	 * 
+	 * 
+	 * 
+	 * A10;S20;W10;D30;X;A1A;B10A11;;A10;
+	 * 
+	 * 
+	 * 
+	 * 处理过程：
+	 * 
+	 * 
+	 * 
+	 * 起点（0,0）
+	 * 
+	 * 
+	 * 
+	 * + A10 = （-10,0）
+	 * 
+	 * 
+	 * 
+	 * + S20 = (-10,-20)
+	 * 
+	 * 
+	 * 
+	 * + W10 = (-10,-10)
+	 * 
+	 * 
+	 * 
+	 * + D30 = (20,-10)
+	 * 
+	 * 
+	 * 
+	 * + x = 无效
+	 * 
+	 * 
+	 * 
+	 * + A1A = 无效
+	 * 
+	 * 
+	 * 
+	 * + B10A11 = 无效
+	 * 
+	 * 
+	 * 
+	 * + 一个空 不影响
+	 * 
+	 * 
+	 * 
+	 * + A10 = (10,-10)
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 结果 （10， -10）
+	 * 
+	 * 
+	 * 输入描述: 一行字符串
+	 * 
+	 * 输出描述: 最终坐标，以,分隔
+	 */
+	public void moveCoordinate() {
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		String[] data = input.split(";");
+		int x = 0, y = 0; // 初始坐标
+		for (int i = 0; i < data.length; i++) {
+			String s = data[i]; // 当前运算
+			if (s.length() < 2 || s.length() > 3) // 空串或者不符合的直接跳过
+				continue;
+			try {
+				if (s.charAt(0) == 'A')
+					x -= Integer.parseInt(s.substring(1));
+				else if (s.charAt(0) == 'D')
+					x += Integer.parseInt(s.substring(1));
+				else if (s.charAt(0) == 'W')
+					y += Integer.parseInt(s.substring(1));
+				else if (s.charAt(0) == 'S')
+					y -= Integer.parseInt(s.substring(1));
+			} catch (Exception e) {
+				// 数字解析异常，说明不符合，直接跳过
+				continue;
+			}
+		}
+		System.out.println(x + "," + y);
+		in.close();
 	}
 
 	/**
